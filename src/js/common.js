54719e6ad92eee48b3e3aeba7fd200c9d486d39f
HorbynkoIvan <horbynko.i@gmail.com>
2020-12-24 15:15:45 +0200

    build

--- src/js/common.js
+++ src/js/common.js
@@ -9,7 +9,7 @@ window.$ = window.jQuery = $;
 
 window.addEventListener('DOMContentLoaded', () => {
     // $(".vichy_ny2021").snowfall({
-    //     image: "https://cdnparftop.parfums.ua/public/upload/newAdminUpload/Landing/NYvichy89/flake1.png",
+    //     image: "https://cdn.parfums.ua/public/upload/newAdminUpload/Landing/NYvichy89/flake1.png",
     //     minSize: 10,
     //     maxSize: 32
     // });
